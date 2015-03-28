1d76a432478c5ae285c0554caeec5d40ab8ac8e9
Verna Koskinen <darth.kipsu@gmail.com>
2015-03-28 11:23:29 +0200

    add initial calendar view

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,16 @@
+var loadCalendar = function() {
+    $('#calendar').fullCalendar({
+        defaultView: 'agendaWeek',
+        firstDay: 1,
+        weekNumbers: true,
+        businessHours: {
+            start: '8:00',
+            end:   '16:00',
+            dow: [ 1, 2, 3, 4, 5]
+        },
+        scrollTime: '7:00',
+        aspectRatio: 1.7
+    });
+}
+$(document).ready(loadCalendar);
+$(document).on('page:load', loadCalendar);
