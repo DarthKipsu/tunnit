0b9e34e11290bd059ffc62b85023af44760030c9
Verna Koskinen <darth.kipsu@gmail.com>
2015-04-16 20:07:58 +0300

    configure pdfkit middleware

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Tunnit
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/app/validators)
+    config.middleware.use PDFKit::Middleware
   end
 end
