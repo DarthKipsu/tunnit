ec0eeee2e46e606bea7d8d93e9b6ae251a49f3fa
Verna Koskinen <darth.kipsu@gmail.com>
2015-04-18 19:50:11 +0300

    fix report pdf paths

--- spec/features/report_generation_spec.rb
+++ spec/features/report_generation_spec.rb
@@ -4,20 +4,18 @@ describe 'Report generation' do
   before(:each) do
     create_user_and_sign_in
     @project = FactoryGirl.create(:project)
-    @event = FactoryGirl.create(:event, project_id: @project.id)
+    @event = FactoryGirl.create(:event, project_id: @project.id, user_id: @user.id)
     @project2 = FactoryGirl.create(:project, name: 'Other project')
-    @event2 = FactoryGirl.create(:event, project_id: @project2.id)
+    @event2 = FactoryGirl.create(:event, project_id: @project2.id, user_id: @user.id)
   end
 
   describe 'creating personal monthly report' do
     before :each do
-      fill_in 'start_time', with: (Time.now - 1.week).to_s
-      fill_in 'end_time', with: (Time.now).to_s
-      click_button 'Generate report'
+      visit "/users/1/report/?start_time=1-1-2015&end_time=1-1-2020"
     end
 
     it 'displays users name on report' do
-      expect(page).to have_content 'Mikko Makkonen'
+      expect(page).to have_content 'Mikko Makkonen (test@test.fi)'
     end
 
     it 'displays correct total hours' do
